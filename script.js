f18516da5b2e861d9261ef707f25815b2c571f45
mmulekya <kizitohamisi61@gmail.com>
2026-02-09 16:39:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ signupBtn.onclick = () => {
 loginBtn.onclick = () => {
   container.classList.remove("active");
 };
+let startX = 0;
+let endX = 0;
+
+const containerEl = document.getElementById("container");
+
+containerEl.addEventListener("touchstart", e => {
+  startX = e.touches[0].clientX;
+});
+
+containerEl.addEventListener("touchend", e => {
+  endX = e.changedTouches[0].clientX;
+  handleSwipe();
+});
+
+function handleSwipe() {
+  if (startX - endX > 50) {
+    // swipe left → signup
+    containerEl.classList.add("active");
+  }
+
+  if (endX - startX > 50) {
+    // swipe right → login
+    containerEl.classList.remove("active");
+  }
+}
